e4c8b6abfa57880f27d0515d543261844e2e3110
robksawyer <robksawyer@gmail.com>
2015-05-19 22:03:23 -0700

    Updated the brenda submodule

--- api/controllers/RenderController.js
+++ api/controllers/RenderController.js
@@ -7,6 +7,25 @@
 
 module.exports = {
 
+	price: function (req, res)
+	{
+		var options = {
+			mode: 'binary',
+			pythonPath: '/usr/local/bin/python', /* If installed with 'brew install python' */
+			pythonOptions: ['-u'],
+			scriptPath: 'lib/brenda/',
+			args: ['']
+		};
+		sails.python.run('brenda-price', options, function (err, results) {
+			if (err) throw err;
+			// results is an array consisting of messages collected during execution
+			sails.log('results: %j', results);
+			res.view({
+				results: results
+			});
+		});
+	},
+
 	status: function (req, res)
 	{
 		var options = {
@@ -24,8 +43,6 @@ module.exports = {
 				results: results
 			});
 		});
-
-		res.view();
 	}
 };
 
