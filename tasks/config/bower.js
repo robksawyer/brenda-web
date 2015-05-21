eb3df4d90d293dcd633f38eb103cabcf27af85fd
robksawyer <robksawyer@gmail.com>
2015-05-21 01:21:04 -0700

    Minor copy update

--- tasks/config/bower.js
+++ tasks/config/bower.js
@@ -4,13 +4,7 @@ module.exports = function(grunt) {
 		dev: {
 			dest: '.tmp/public',
 			js_dest: '.tmp/public/js',
-			css_dest: '.tmp/public/styles',
-			//eot_dest: '.tmp/public/styles/fonts',
-			//svg_dest: '.tmp/public/styles/fonts',
-			//woff_dest: '.tmp/public/styles/fonts',
-			//woff2_dest: '.tmp/public/styles/fonts',
-			//otf_dest: '.tmp/public/styles/fonts',
-			//ttf_dest: '.tmp/public/styles/fonts',
+			css_dest: '.tmp/public/styles'
 		}
 	});
 
