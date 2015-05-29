e2c41085a5be93bfc5867c23ac898358303aacf8
robksawyer <robksawyer@gmail.com>
2015-05-28 21:26:39 -0700

    Cleaned up the upload code

--- api/controllers/JobsController.js
+++ api/controllers/JobsController.js
@@ -6,10 +6,7 @@
  */
 
 var util = require('util'),
-	path = require('path'),
-	fs = require('fs'),
-	AWS = require('aws-sdk'),
-	zlib = require('zlib');
+	path = require('path');
 
 module.exports = {
 
@@ -55,56 +52,11 @@ module.exports = {
 						//Check to see if a Blender file was uploaded
 						if(typeof req.file('project_file') !== 'undefined'){
 
-							var upload = req.file('project_file')._files[0].stream,
-								upload_headers = upload.headers,
-								upload_byteCount = upload.byteCount,
-								upload_filename = upload.filename,
-								upload_extension = path.extname(upload_filename),
-								upload_validated = true,
-								upload_fileParams = {},
-								upload_settings = {
-									allowedTypes: [
-										'application/zip',
-										'application/octet-stream',
-										'application/x-gzip',
-										'multipart/x-gzip',
-										'multipart/x-zip',
-										'application/blender'
-									],
-									maxBytes: 100 * 1024 * 1024
-								};
-
-							var allowedExtensions = ['.zip','.gz','.gzip'];
-
-							var projectTmpFolder = path.join(process.cwd() , '.tmp', 'uploads');
-							var tmpPath = sails.os.tmpdir();
-
-							//var targetPathWithFilename = path.resolve( uploadedFiles[0].fd );
-							var filenameWithoutExt = upload_filename.replace(upload_extension, '');
-							var destPath = path.join(projectTmpFolder);
-							var destPathWithFilename = path.join(projectTmpFolder, filenameWithoutExt + '.zip');
-							var outputZipFilename = filenameWithoutExt + '.zip';
-
-							sails.log('Project tmp: ' + projectTmpFolder);
-							sails.log('OS tmp: ' + tmpPath);
-							sails.log("Filename w/o ext: " + filenameWithoutExt);
-							//sails.log.info("Target path: " + targetPathWithFilename);
-							sails.log.info("Destination path w/Filename: " + destPathWithFilename);
-							sails.log.info("Destination path: " + destPath);
-
-							// Check file type
-							if (_.indexOf(upload_settings.allowedTypes, upload_headers['content-type']) === -1) {
-								upload_validated = false;
-								errors.push({message: 'Wrong filetype (' + upload_headers['content-type'] + ').'});
-							}
-							// Check file size
-							if (upload_byteCount > upload_settings.maxBytes) {
-								upload_validated = false;
-								errors.push({message: 'Filesize exceeded: ' + upload_byteCount + '/' + upload_settings.maxBytes + '.'});
-							}
+							var fileStream = req.file('project_file')._files[0].stream;
+							errors = uploader.validate(fileStream);
 
 							// Upload the file.
-							if (upload_validated) {
+							if (errors.length < 1) {
 								sails.log.info('File passed validation!');
 
 								//Check to see if the file has a .blend, .gz, or .zip extension.
@@ -112,104 +64,23 @@ module.exports = {
 								if(upload_extension == '.blend'){
 									sails.log.info('Blender file found. Zipping the file before uploading to S3.');
 
-									req.file('project_file').upload({
-										dirname: destPath,
-										//saveAs: upload_filename
-									}, function whenDone(err, uploadedFiles){
-										if(err) return res.negotiate(err);
+									//Create the zip
+									uploader.createZipAndUploadToS3(fileStream).then(
+										function(data){
+											sails.log('Promised fullfilled.');
+											sails.log.info(data);
 
-										var uploadedFileLocalPath = uploadedFiles[0].fd;
-										//
-										// Zip the file up and then send to S3
-										//
-										// sails.log('AWS Configuration Information');
-										// sails.log(sails.config.aws.credentials.accessKeyId);
-										// sails.log(sails.config.aws.credentials.secretAccessKey);
-										// sails.log(settings.aws_s3_project_bucket);
-										// sails.log(settings.aws_s3_region);
-
-										//Setup knox (our Amazon S3 client)
-										//@url https://www.npmjs.com/package/knox
-										/*var streamClient = knox.createClient({
-											key: sails.config.aws.credentials.accessKeyId,
-											secret: sails.config.aws.credentials.secretAccessKey,
-											bucket: settings.aws_s3_project_bucket,
-											region: settings.aws_s3_region
-										});*/
-
-										AWS.config.update(sails.config.aws.credentials);
-
-										var readStream = fs.createReadStream(uploadedFileLocalPath);
-										var s3Stream = require('s3-upload-stream')(new AWS.S3());
-										var s3UploadStream = s3Stream.upload({
-														Bucket: settings.aws_s3_project_bucket,
-														Key: filenameWithoutExt + '.gz', //The filename
-														ACL: "authenticated-read",
-														StorageClass: "REDUCED_REDUNDANCY",
-														//ContentType: upload_headers['content-type']
-													});
-
-										var compress = zlib.createGzip();
-
-										// Optional configuration
-										//s3UploadStream.maxPartSize(20971520); // 20 MB
-										//s3UploadStream.concurrentParts(5);
-
-										// Handle errors.
-										s3UploadStream.on('error', function (error) {
-											sails.log(error);
-										});
-
-										/* Handle progress. Example details object:
-										   { ETag: '"f9ef956c83756a80ad62f54ae5e7d34b"',
-										     PartNumber: 5,
-										     receivedSize: 29671068,
-										     uploadedSize: 29671068 }
-										*/
-										s3UploadStream.on('part', function (details) {
-											sails.log(details);
-										});
-
-										/* Handle upload completion. Example details object:
-										   { Location: 'https://bucketName.s3.amazonaws.com/filename.ext',
-										     Bucket: 'bucketName',
-										     Key: 'filename.ext',
-										     ETag: '"bf2acbedf84207d696c8da7dbb205b9f-5"' }
-										*/
-										s3UploadStream.on('uploaded', function (details) {
-											sails.log(details);
-										});
-
-										readStream.pipe(compress).pipe(s3UploadStream);
-
-										/*var streamReq = streamClient.put(, {
-											'Content-Length': headers['content-length'],
-											'Content-Type': headers['content-type'],
-											'x-amz-acl': 'public-read'
-										});*/
-
-										/*streamReq.on('response', function(res){
-											if (200 == res.statusCode) {
-												sails.log('saved to %s', req.url);
+											return res.view('jobs/add_spot',{
+												error: errors,
+												file: data
+											});
+										},
+										function(error){
+											sails.log.error(error);
+											if(err) {
+												return res.negotiate(error);
 											}
-										});*/
-
-										//Create the zip
-										/*brenda.createZip(res, filenameWithoutExt, targetPathWithFilename, destPathWithFilename,'Created with Brenda web app.').then(
-											function(data){
-												sails.log.info(data);
-
-												res.view('jobs/add_spot',{
-													error: errors,
-													file: data
-												});
-											},
-											function(error){
-												sails.log.error(error);
-												if(err) return res.negotiate(error);
-											}
-										);*/
-									});
+										});
 
 								} else if(allowedExtensions.indexOf(extension) > -1) {
 									//
@@ -275,12 +146,7 @@ module.exports = {
 
 				} //method check
 
-				if(errors.length < 1){
-					res.view('jobs/add_spot',{
-						settings: settings,
-						error: errors
-					});
-				} else {
+				if(errors.length > 0){
 					res.view('jobs/add_spot',{
 						settings: settings,
 						error: errors
--- api/services/uploader.js
+++ api/services/uploader.js
@@ -4,9 +4,152 @@
  * @description :: Server-side logic for managing files
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-var path = require('path');
+var path = require('path'),
+	os = require('os'),
+	fs = require('fs'),
+	AWS = require('aws-sdk'),
+	zlib = require('zlib'),
+	s3Stream = require('s3-upload-stream')(new AWS.S3());
 
 module.exports = {
+
+	/**
+	*
+	* Handles validating the files uploaded
+	* @param fileStream: object - File stream from the upload
+	* @param allowedExtensions: array - An array of extensions to validated based on. Be sure to include the '.' e.g, .blend in the extension.
+	* @param maxBytes: integer - The maximum number of bytes to allow
+	* @return object
+	**/
+
+	validate: function(fileStream, allowedExtensions, maxBytes){
+
+		if(!allowedExtensions) {
+			allowedExtensions = ['.zip','.gz','.gzip'];
+		}
+
+		var upload = fileStream,
+			headers = upload.headers,
+			byteCount = upload.byteCount,
+			filename = upload.filename,
+			extension = path.extname(filename),
+			validated = true,
+			fileParams = {},
+			settings = {
+				allowedTypes: [
+					'application/zip',
+					'application/octet-stream',
+					'application/x-gzip',
+					'multipart/x-gzip',
+					'multipart/x-zip',
+					'application/blender'
+				],
+				maxBytes: 100 * 1024 * 1024
+			};
+
+		if(maxBytes){
+			settings.maxBytes = maxBytes;
+		}
+
+		var errors = [];
+
+		var projectTmpFolder = path.join(process.cwd() , '.tmp', 'uploads');
+		var tmpPath = os.tmpdir();
+
+		var filenameWithoutExt = filename.replace(extension, '');
+		var destPath = path.join(projectTmpFolder);
+		var destPathWithFilename = path.join(projectTmpFolder, filenameWithoutExt + '.zip');
+		var outputZipFilename = filenameWithoutExt + '.zip';
+
+		sails.log('Project tmp: ' + projectTmpFolder);
+		sails.log('OS tmp: ' + tmpPath);
+		sails.log("Filename w/o ext: " + filenameWithoutExt);
+		sails.log.info("Destination path w/Filename: " + destPathWithFilename);
+		sails.log.info("Destination path: " + destPath);
+
+		// Check file type
+		if (_.indexOf(settings.allowedTypes, headers['content-type']) === -1) {
+			validated = false;
+			errors.push({message: 'Wrong filetype (' + headers['content-type'] + ').'});
+		}
+		// Check file size
+		if (byteCount > settings.maxBytes) {
+			validated = false;
+			errors.push({message: 'Filesize exceeded: ' + byteCount + '/' + settings.maxBytes + '.'});
+		}
+
+		return errors;
+	},
+
+	/**
+	*
+	* Creates a zip file and uploads the file to Amazon S3
+	* @param fileStream: object - The upload file stream
+	* @return promise
+	**/
+	createZipAndUploadToS3: function(fileStream){
+		var promise = new sails.RSVP.Promise( function(fullfill, reject) {
+
+			var filename = fileStream.filename,
+				extension = path.extname(filename),
+				filenameNoExt = filename.replace(extension, '');
+
+			var readStream = fs.createReadStream(fileStream);
+			sails.log('readStream');
+			sails.log(readStream);
+
+			var s3Stream = require('s3-upload-stream')(new AWS.S3());
+
+			//Update the credentials with the local ones
+			AWS.config.update(sails.config.aws.credentials);
+
+			var s3UploadStream = s3Stream.upload({
+							Bucket: settings.aws_s3_project_bucket,
+							Key: filenameNoExt + '.zip', //filename
+							ACL: "authenticated-read",
+							StorageClass: "REDUCED_REDUNDANCY"
+						});
+
+			var compress = zlib.createGzip();
+
+			// Optional configuration
+			//s3UploadStream.maxPartSize(20971520); // 20 MB
+			//s3UploadStream.concurrentParts(5);
+
+			// Handle errors.
+			s3UploadStream.on('error', function (error) {
+				sails.log(error);
+				reject(error);
+			});
+
+			/* Handle progress. Example details object:
+			   { ETag: '"f9ef956c83756a80ad62f54ae5e7d34b"',
+			     PartNumber: 5,
+			     receivedSize: 29671068,
+			     uploadedSize: 29671068 }
+			*/
+			s3UploadStream.on('part', function (details) {
+				sails.log(details);
+			});
+
+			/* Handle upload completion. Example details object:
+			   { Location: 'https://bucketName.s3.amazonaws.com/filename.ext',
+			     Bucket: 'bucketName',
+			     Key: 'filename.ext',
+			     ETag: '"bf2acbedf84207d696c8da7dbb205b9f-5"' }
+			*/
+			s3UploadStream.on('uploaded', function (details) {
+				sails.log(details);
+				//Fullfill the promise
+				fullfill(details);
+			});
+
+			//Kick off the process
+			readStream.pipe(compress).pipe(s3UploadStream);
+
+		});
+		return promise;
+	},
 
 	/**
 	 * `FileController.upload()`
@@ -25,29 +168,30 @@ module.exports = {
 			res.setTimeout(0);
 			req.file('files[]')
 			.upload({
-
 				// You can apply a file upload limit (in bytes)
 				maxBytes: 1000000,
-
 				dirname: path.resolve('', 'files/projects')
 			}, function whenDone(err, uploadedFiles) {
+
 				var filenameContainer = uploadedFiles[0].fd.split('/')
-						filename          = filenameContainer[filenameContainer.length - 1 ];
-						filelocation      = '/projects/' + filename;
-				console.log(filename);
+							 filename = filenameContainer[filenameContainer.length - 1 ];
+						 filelocation = '/projects/' + filename;
+
+				sails.log(filename);
 				var temp = {};
-				temp.files = [
-					{
+				temp.files = [{
 						"name": uploadedFiles.filename,
 						"size": uploadedFiles.size,
 						"url": filelocation,
 						"thumbnailUrl": filelocation
-					}
-				];
+					}];
 
-				console.log(uploadedFiles);
-				if (err) reject(err);
-				else fullfill(temp);
+				sails.log(uploadedFiles);
+				if (err) {
+					reject(err);
+				} else {
+					fullfill(temp);
+				}
 			});
 		});
 		return promise;
