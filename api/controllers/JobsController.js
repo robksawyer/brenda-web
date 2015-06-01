9418af7c643f6e6fdc600748d07160c56994c12a
robksawyer <robksawyer@gmail.com>
2015-06-01 11:14:58 -0700

    Cleaned up the error handling in the spot instance method a bit

--- api/controllers/JobsController.js
+++ api/controllers/JobsController.js
@@ -28,121 +28,114 @@ module.exports = {
 		//Pull the user's settings from the database
 		brenda.getUserSettings(req).then( function(settings) {
 
-				sails.log(req.params.all());
+			sails.log(req.params.all());
 
-				var errors = [];
+			var errors = [];
 
-				if (req.method == 'POST') {
+			if (req.method == 'POST') {
 
-					if(!sails.config.aws.credentials.accessKeyId){
-						errors.push({message: 'You must provide a valid AWS access key.'});
-					}
-					if(!sails.config.aws.credentials.secretAccessKey){
-						errors.push({message: 'You must provide a valid AWS secret access key.'});
-					}
-					if(!settings.aws_s3_project_bucket){
-						errors.push({message: 'You have not configured an AWS S3 project bucket. Do this on the <a href="/settings">settings page</a>.'});
-					}
-					if(!settings.aws_s3_region){
-						errors.push({message: 'You have not configured a AWS S3 region. Do this on the <a href="/settings">settings page</a>.'});
-					}
+				if(!sails.config.aws.credentials.accessKeyId){
+					errors.push({message: 'You must provide a valid AWS access key.'});
+				}
+				if(!sails.config.aws.credentials.secretAccessKey){
+					errors.push({message: 'You must provide a valid AWS secret access key.'});
+				}
+				if(!settings.aws_s3_project_bucket){
+					errors.push({message: 'You have not configured an AWS S3 project bucket. Do this on the <a href="/settings">settings page</a>.'});
+				}
+				if(!settings.aws_s3_region){
+					errors.push({message: 'You have not configured a AWS S3 region. Do this on the <a href="/settings">settings page</a>.'});
+				}
 
-					if(errors.length < 1) {
+				if(errors.length < 1) {
 
-						//Check to see if a Blender file was uploaded
-						if(typeof req.file('project_file') !== 'undefined') {
+					//Check to see if a Blender file was uploaded
+					if(typeof req.file('project_file') !== 'undefined') {
 
-							var fileStream = req.file('project_file')._files[0].stream,
-								filename = fileStream.filename,
-								extension = path.extname(filename),
-								allowedExtensions = ['.zip','.gz'];
+						var fileStream = req.file('project_file')._files[0].stream,
+							filename = fileStream.filename,
+							extension = path.extname(filename),
+							allowedExtensions = ['.zip','.gz'];
 
-							errors = uploader.validate(fileStream);
+						errors = uploader.validate(fileStream);
 
-							// Upload the file.
-							if (errors.length < 1) {
-								sails.log.info('File passed validation!');
+						// Upload the file.
+						if (errors.length < 1) {
+							sails.log.info('File passed validation!');
 
-								//Check to see if the file has a .blend, .gz, or .zip extension.
-								//
-								if(extension == '.blend'){
-									sails.log.info('Blender file found. Zipping the file before uploading to S3.');
+							//Check to see if the file has a .blend, .gz, or .zip extension.
+							//
+							if(extension == '.blend'){
+								sails.log.info('Blender file found. Zipping the file before uploading to S3.');
 
-									//Process the file input
-									brenda.processBlenderFile(req, fileStream, settings).then(
-										function(jobRecord){
-											req.flash('success', 'Spot instance job ' + jobRecord.name + ' created successfully!');
-											res.redirect('jobs/submit/' + jobRecord.id);
-										},
-										function(err){
-											return res.negotiate(err);
-										}
-									);
+								//Process the file input
+								brenda.processBlenderFile(req, fileStream, settings).then(
+									function(jobRecord){
+										req.flash('success', 'Spot instance job ' + jobRecord.name + ' created successfully!');
+										res.redirect('jobs/submit/' + jobRecord.id);
+									},
+									function(err){
+										req.flash('error', err);
+										return res.negotiate(err);
+									}
+								);
 
-								} else if(allowedExtensions.indexOf(extension) > -1) {
+							} else if(allowedExtensions.indexOf(extension) > -1) {
+								sails.log.info('Compressed file found. Uploading to S3.');
 
-									//Process the file input
-									brenda.processZipFile( req, settings ).then(
-										function(fileRecord){
-											res.view('jobs/add_spot',{
-												error: errors,
-												settings: settings,
-												file: fileRecord
-											});
-										},
-										function(err){
-											req.flash('message', reason);
-											return res.negotiate(err);
-										}
-									);
+								//Process the file input
+								brenda.processZipFile( req, settings ).then(
+									function(fileRecord){
+										req.flash('success', 'Spot instance job ' + jobRecord.name + ' created successfully!');
+										res.redirect('jobs/submit/' + jobRecord.id);
+									},
+									function(err){
+										req.flash('error', err);
+										return res.negotiate(err);
+									}
+								);
 
-								} else {
-									req.flash('message', 'Unable to find the extension of the file.');
-									errors.push([{message: 'Unable to find the extension of the file.'}]);
-									res.view('jobs/add_spot',{
-										settings: settings,
-										error: errors
-									});
-								}
 							} else {
-								req.flash('message', 'File validation failed.');
-								errors.push([{message: 'File validation failed.'}]);
+								req.flash('error', 'Unable to find the extension of the file.');
 								res.view('jobs/add_spot',{
-									settings: settings,
-									error: errors
+									settings: settings
 								});
 							}
 						} else {
-							req.flash('message', 'The file could not be found or was not uploaded.');
-							errors.push({message: 'The file could not be found or was not uploaded.'});
+							req.flash('error', 'File validation failed.');
 							res.view('jobs/add_spot',{
-								settings: settings,
-								error: errors
+								settings: settings
 							});
 						}
 					} else {
-
+						req.flash('error', 'The file could not be found or was not uploaded.');
 						res.view('jobs/add_spot',{
-							settings: settings,
-							error: errors
+							settings: settings
 						});
-
 					}
 				} else {
 
-					//Other requests
 					res.view('jobs/add_spot',{
-						settings: settings,
-						error: errors
+						settings: settings
 					});
 
 				}
+			} else {
 
-			},
-			function(error){
-				sails.log.error(error);
-				return res.notFound();
-			});
+				//Other requests
+				res.view('jobs/add_spot',{
+					settings: settings,
+					error: errors
+				});
+
+			}
+
+		},
+		function(err){
+			sails.log.error(err);
+			req.flash('error', err);
+			return res.notFound();
+		});
 
 	},
 
