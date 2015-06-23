3a6012b7498cfccc9c284ec95f9647ca39075c75
robksawyer <robksawyer@gmail.com>
2015-06-22 23:17:22 -0700

    Fixed some issues with the models. And hopefully fixed #23

---
+++ api/controllers/UploadController.js
@@ -0,0 +1,11 @@
+/**
+ * UploadController
+ *
+ * @description :: Server-side logic for managing Uploads
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
